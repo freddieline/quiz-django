61a8a5b9c608bc34fdfbc13fbf80e24406cf5f88
Freddie Line <freddieline@hotmail.com>
2025-01-13 14:31:19 +0000

    fix: reduce slug size

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,9 +2,27 @@
 from rest_framework.routers import DefaultRouter
 from .views import QuizQuestionViewSet
 from .views import QuizViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'quiz-questions', QuizQuestionViewSet)
 router.register(r'quizzes', QuizViewSet)
 
-urlpatterns = router.urls
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="API documentation for my Django project",
+      contact=openapi.Contact(email="contact@myapi.com"),
+   ),
+   public=True,
+)
+
+
+urlpatterns = [
+    path('', include(router.urls)), 
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-schema'),
+]
--- quiz/views.py
+++ quiz/views.py
@@ -7,6 +7,7 @@ from .serializers import QuizQuestionSerializer
 from .serializers import QuizSerializer
 from .filters import QuizQuestionFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import ValidationError
 
 class QuizQuestionViewSet(viewsets.ModelViewSet):
     queryset = QuizQuestion.objects.all()
@@ -15,7 +16,27 @@ class QuizQuestionViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'patch', 'delete'] 
     filterset_class = QuizQuestionFilter
 
+    def create(self, request, *args, **kwargs):
+        if not request.data:
+            raise ValidationError("Request body cannot be empty.")
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not request.data:
+            raise ValidationError("Request body cannot be empty.")
+        return super().update(request, *args, **kwargs)
+
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']  # Allows both PUT and PATCH
+
+    def create(self, request, *args, **kwargs):
+        if not request.data:
+            raise ValidationError("Request body cannot be empty.")
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not request.data:
+            raise ValidationError("Request body cannot be empty.")
+        return super().update(request, *args, **kwargs)
